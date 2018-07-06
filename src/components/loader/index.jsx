0bf070f6de9bae0b4ac880f659177aa89f1fe4fb
AbdelfatahAtef <abdelfatah.atef94@gmail.com>
2018-07-07 01:28:20 +0200

    import all files

---
+++ src/components/loader/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./loader.sass";
+
+function Loader() {
+  return (
+    <div className="loader">
+      <div className="spinner" />
+    </div>
+  );
+}
+export default Loader;
---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import List from "./list";
+import Filter from "./filter";
+import "./sidebar.sass";
+
+function SideBar({ openSideBar }) {
+    const isMobile = window.innerWidth <= 768;
+    const sideBarDesktop = isMobile ? 'sidebar--mobile' : '';
+  return (
+    <div className="col-md-3  no-padding-left">
+      <section className={`sidebar ${openSideBar ? "sidebar--opened" : ""} ${sideBarDesktop}`}>
+        <img
+          src="http://via.placeholder.com/300x50"
+          alt="logo"
+          className="sidebar__logo hidden-xs hidden-sm"
+        />
+        <div className="sidebar__content">
+          <List />
+          <Filter />
+        </div>
+      </section>
+    </div>
+  );
+}
+export default SideBar;
