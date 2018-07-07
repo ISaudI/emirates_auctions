6de8133ab8994895c3a5c2596d1d7c4854087c6c
AbdelfatahAtef <abdelfatah.atef94@gmail.com>
2018-07-07 12:58:16 +0200

    add constants file

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export default {
+  MOBILE_BREAKPOINT: 1024,
+  LOGO_IMAGE: "http://via.placeholder.com/300x50",
+  USER_NAME: "Abdelfattah",
+  ACCOUNT_IMAGE:
+    "https://cdn.emiratesauction.com/media/9jeqvldvobggms4ko3qmdfpdqq0s12o81i3wid3o67xji1to3/t_,w_0,h_0/images.jpg"
+};
